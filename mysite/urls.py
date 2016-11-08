40b4a63a355c397c9555e8423ca593c45e667ac9
lungen <lungenfetzer@gmail.com>
2016-11-08 22:22:34 +0000

    HTML changes

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
+    # Django will now redirect everything that comes into
+    # 'http://127.0.0.1:8000/' to blog.urls
 ]
