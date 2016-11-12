93e966ebfa43e516cde4a91fe36a46b04be912fc
lungen <lungenfetzer@gmail.com>
2016-11-12 10:29:12 +0000

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    # (?P<pk>\d+) this part is trickier. It means that Django will take
+    # everything that you place here and transfer it to a view as a variable
+    # called pk. \d also tells us that it can only be a digit, not a letter (so
+    # everything between 0 and 9). + means that there needs to be one or more
+    # digits there.
+    # pk is an abbreviation for primary key. This name is often used in Django
+    # projects.
 ]
